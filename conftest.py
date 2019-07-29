66bd711910437a2acab1e90601ad0524e30a9504
Slava Kamrad <slavakamrad@gmail.com>
2019-07-29 13:56:16 +0300

    Update conftest.py
    Add browser change in conftest.py

--- conftest.py
+++ conftest.py
@@ -3,24 +3,27 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default='es',
                      help="Choose lang")
 
-
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    browser = None
+    user_language = request.config.getoption("language")
     if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        print("\nstart browser chrome for test...")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        print("\nstart browser firefox for test...")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        print("Browser {} still is not implemented".format(browser_name))
     yield browser
-    print("\nquit browser..")
+    print("\nquit browser...")
     browser.quit()
