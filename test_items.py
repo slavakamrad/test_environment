39c49cd823dacb028c848b9e896011a8209ae925
Slava Kamrad <slavakamrad@gmail.com>
2019-07-29 13:57:08 +0300

    Update test_items.py
    test_items.py has changed

--- test_items.py
+++ test_items.py
@@ -1,10 +1,7 @@
 import time
 
-link = "ttp://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
 def test_find_add_to_card_btn(browser):
-    browser.get(link)
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     time.sleep(30)
     assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), \
         'Кнопка добавления товара в корзину отсутсвует'
-    
