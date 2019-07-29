fa35392d0b3689543d2a1580ded633eee2c75e81
Slava Kamrad <slavakamrad@gmail.com>
2019-07-29 20:55:08 +0300

    Update test_items.py
    Add comment "30 sec wait"

--- test_items.py
+++ test_items.py
@@ -2,6 +2,6 @@ import time
 
 def test_find_add_to_cart_btn(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    time.sleep(30)
+    #time.sleep(30)
     assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), \
         "Button Add_to_cart doesn't exist"
