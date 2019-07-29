452b2f6e44725a78fcef98c39dc1edd571e0ab46
Slava Kamrad <slavakamrad@gmail.com>
2019-07-29 13:05:31 +0300

    Create test_items.py
    Create project file test_items.py

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+
+link = "ttp://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_find_add_to_card_btn(browser):
+    browser.get(link)
+    time.sleep(30)
+    assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), \
+        'Кнопка добавления товара в корзину отсутсвует'
+    
