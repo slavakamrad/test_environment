602571fd1bdea4033fd13f7a6434d0b8d0e8eb4d
Slava Kamrad <slavakamrad@gmail.com>
2019-07-29 13:59:05 +0300

    Update test_items.py
    Check code in test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 
-def test_find_add_to_card_btn(browser):
+def test_find_add_to_cart_btn(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     time.sleep(30)
     assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), \
-        'Кнопка добавления товара в корзину отсутсвует'
+        "Button Add_to_cart doesn't exist"
